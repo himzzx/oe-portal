e06deabe7239e28ce2e083a8568acdd4e066fe27
himzzx <ksd.him45@gmail.com>
2026-03-18 14:45:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from flask import Flask
-app = Flask(__name__)
-
+app = Flask(name)
 @app.route("/")
 def home():
-return "<h1>Welcome to OE Portal 🚀</h1>"
-
-if name == "main":
-app.run(host="0.0.0.0", port=8080)
+return "Welcome to OE Portal 🚀"
